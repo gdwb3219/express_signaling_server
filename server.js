ae6c7e939fff4f7c0c9f515af30b749e43b6eb98
gdwb3219 <gdwb3219@gmail.com>
2024-05-15 22:46:27 +0900

    통신 전달 로그 추가

--- server.js
+++ server.js
@@ -8,15 +8,15 @@ const wss = new WebSocket.Server({ server });
 
 wss.on("connection", function connection(ws) {
   console.log("Client has Connected");
-  console.log(wss.clients);
+  // console.log("\nClients 확인",wss.clients);
   ws.on("message", function incoming(message) {
     // 프론트엔드에서 받은 Data 확인
     const data = JSON.parse(message);
-    console.log(data, "Message 받은 것 같다.");
+    // console.log(data, "Message 받은 것 같다.");
 
     switch (data.type) {
       case "offer":
-        console.log("Received an offer:", data.offer);
+        console.log("\nReceived an offer:", data.offer);
         break;
       case "answer":
         console.log("Received an answer:", data.answer);
@@ -39,12 +39,12 @@ wss.on("connection", function connection(ws) {
   // 연결이 끊기는 경우 처리
   ws.on("close", () => {
     console.log("Client has disconnected");
+    console.log("_______________________");
   });
 });
 
 // Express를 사용하여 웹 서버에서 정적 파일 호스팅 또는 API 라우팅을 설정
 app.get("/", (req, res) => {
-  console.log("WEB 연결 클라이언트", wss.clients);
   res.send("Hello World! This is the WebRTC signaling server.");
 });
 
